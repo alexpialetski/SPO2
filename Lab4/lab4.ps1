7cfce8c5ed764c37894f3a76831b4df15004329d
Aliaksei Pialetski <Aliaksei_Pialetski@epam.com>
2020-02-24 17:03:07 +0300

    Lab4

---
+++ Lab4/lab4.ps1
@@ -0,0 +1,11 @@
+﻿$choice = Read-Host -Prompt 'Enter your choice'
+New-Item -Path $choice -ItemType Directory  
+$result=fsutil fsinfo drives
+$drives = $result[1].split(' ')
+For($i=1; $i -lt $drives.Length; $i++ ){
+  dir $drives[$i] >> $choice"\report.txt"
+}
+Get-ComputerInfo >> $choice"\report.txt"
+cd $choice
+Write-Output "File in"
+Get-Location
