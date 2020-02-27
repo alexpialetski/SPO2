2dededa747bec5867830cdbbd4eb74f39fd9444e
Aliaksei Pialetski <Aliaksei_Pialetski@epam.com>
2020-02-27 11:13:19 +0300

    Lab3

--- Lab3/lab3.ps1
+++ Lab3/lab3.ps1
@@ -1,55 +1,17 @@
-﻿Write-Output('F - Enter FIO')
-Write-Output('N - Enter phone number')
-Write-Output('G - Enter group number')
-Write-Output('D - Enter birth date')
-
-$choice = Read-Host -Prompt 'Enter your choice'
-
-If($choice -eq 'F'){
-  $fchoice = Read-Host -Prompt 'Enter surname'
-  $groupResult=findInGroup($fchoice)
-  ForEach-Object -InputObject $groupResult -Process {
-    $name=$_.split(' ')[0]
-    $numberResult=findInNumber($fchoice)
-    $birthResult=findInBirth($fchoice)
-  }
-} ElseIf($choice -eq 'N'){
-
-} ElseIf($choice -eq 'G'){
-
-} ElseIf($choice -eq 'D'){
-
-} Else {
-    exit 0;
-}
-
-printResult($groupResult, $birthResult, $numberResult)
-
-function printResult($groupResult, $birthResult, $numberResult){
-   ForEach-Object -InputObject $groupResult -Process {
-     $fio = $_.split(' ')[0];
-     $group = $_.split(' ')[1];
-     $birth = findBySurname($birthResult, $fio)
-     $number = findBySurname($numberResult, $fio)
-     Write-Host $fio" - "$group" - "$birth" - "$number
-   }
-}
-
-function findBySurname($fromFile, $name){
-  if($fromFile.GetType().name -ne 'Object[]'){
-    $fromFile=@($fromFile)
-  }
-  $result=@("")
-  ForEach-Object -InputObject $fromFile -Process { 
-    if($_ -match $name){
-      $result = $result + $_
-    }
-  }
-  if($result.Count -eq 1){
+﻿function findBySurname($fromFile, $name){
+  if(!$fromFile -Or ($fromFile -is [array] -And $fromFile.Length -eq 0)){
     return ''
-  } else {
-    return $result[1].split(' ')[1]
-  }  
+  }
+  if($fromFile -is [array]){
+    $row = $fromFile[0]
+  }else {
+    $row = $fromFile
+  }
+
+  if($row -match $name){
+    return $row.split(' ')[1]
+  }
+  return ''
 }
 
 function findInGroup($pattern){
@@ -63,3 +25,65 @@ function findInNumber($pattern){
 function findInBirth($pattern){
   return Get-Content -Path birth.txt | Where-Object {$_ -match $pattern}
 }
+
+Write-Output('F - Enter FIO')
+Write-Output('N - Enter phone number')
+Write-Output('G - Enter group number')
+Write-Output('D - Enter birth date')
+
+$choice = Read-Host -Prompt 'Enter your choice'
+
+If($choice -eq 'F'){
+  $fchoice = Read-Host -Prompt 'Enter surname'
+  $numberResult=findInNumber($fchoice)
+  foreach($result in $numberResult){
+    $name=$result.split(' ')[0]
+    $number=$result.split(' ')[1]
+    $groupResult=findInGroup($name)
+    $birthResult=findInBirth($name)
+    $group = findBySurname($groupResult)
+    $birth = findBySurname($birthResult)
+    Write-Host $name": "$number", "$group", "$birth
+  }
+} ElseIf($choice -eq 'N'){
+  $nchoice = Read-Host -Prompt 'Enter number'
+  $numberResult=findInNumber($nchoice)
+  foreach($result in $numberResult){
+    $name=$result.split(' ')[0]
+    $number=$result.split(' ')[1]
+    $groupResult=findInGroup($name)
+    $birthResult=findInBirth($name)
+    $group = findBySurname($groupResult)
+    $birth = findBySurname($birthResult)
+    Write-Host $name": "$number", "$group", "$birth
+  }
+} ElseIf($choice -eq 'G'){
+  $gchoice = Read-Host -Prompt 'Enter group'
+  $groupResult=findInGroup($gchoice)
+  foreach($result in $groupResult){
+    $name=$result.split(' ')[0]
+    $group=$result.split(' ')[1]
+    $numberResult=findInNumber($name)
+    $birthResult=findInBirth($name)
+    $number = findBySurname($numberResult)
+    $birth = findBySurname($birthResult)
+    Write-Host $name": "$number", "$group", "$birth
+  }
+} ElseIf($choice -eq 'D'){
+  $bchoice = Read-Host -Prompt 'Enter birth date'
+  $birthResult=findInBirth($bchoice)
+  foreach($result in $birthResult){
+    $name=$result.split(' ')[0]
+    $birth=$result.split(' ')[1]
+    $numberResult=findInNumber($name)
+    $groupResult=findInGroup($name)
+    $number = findBySurname($numberResult)
+    $group = findBySurname($groupResult)
+    Write-Host $name": "$number", "$group", "$birth
+  }
+} Else {
+  Write-Host 'Wrong choice!'
+  exit 0;
+}
+
+Read-Host -Prompt 'Enter!'
