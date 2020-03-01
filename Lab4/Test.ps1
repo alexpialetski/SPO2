ecbc2f02eef458675660e18c8ef999e2ab1018a9
Aliaksei Pialetski <Aliaksei_Pialetski@epam.com>
2020-03-01 17:55:04 +0300

    Tests

---
+++ Lab4/Test.ps1
@@ -0,0 +1,8 @@
+﻿$fchoice = 'Pialetski'
+$groupResult=findInGroup($fchoice)
+Write-Host $groupResult
+
+function findInGroup($pattern){
+  return Get-Content -Path group.txt | Where-Object {$_ -match $pattern}
+}
+
---
+++ Lab4/Test2.ps1
@@ -0,0 +1,20 @@
+﻿$fchoice = 'Pialetski'
+$groupResult=findInGroup($fchoice)
+$numberResult=findInNumber($fchoice)
+$birthResult=findInBirth($fchoice)
+Write-Host $groupResult
+Write-Host $numberResult
+Write-Host $birthResult
+
+function findInGroup($pattern){
+  return Get-Content -Path group.txt | Where-Object {$_ -match $pattern}
+}
+
+function findInNumber($pattern){
+  return Get-Content -Path number.txt | Where-Object {$_ -match $pattern}
+}
+
+function findInBirth($pattern){
+  return Get-Content -Path birth.txt | Where-Object {$_ -match $pattern}
+}
+
